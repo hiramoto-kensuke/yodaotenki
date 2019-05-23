d48c911d192eaf4349f4d09f8d97757207081d7d
hiramoto-kensuke <hiramoto.kensuke@moneyforward.co.jp>
2019-05-23 15:54:07 +0900

    プッシュのテスト5

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -17,31 +17,44 @@ class LinebotController < ApplicationController
       end
     end
 
-    #test用
-    url = "https://www.drk7.jp/weather/xml/13.xml"
-    #utf8に変換して取得(kconv)
-    xml = open(url).read.toutf8
-    doc = REXML::Document.new(xml)
-    #東京都 -> 東京地方の情報取得
-    xpath = 'weatherforecast/pref/area[4]/info'
-    weather = doc.elements[xpath + '/weather'].text
-    per06to12 = doc.elements[xpath + '/rainfallchance/period[2]'].text
-    per12to18 = doc.elements[xpath + '/rainfallchance/period[3]'].text
-    per18to24 = doc.elements[xpath + '/rainfallchance/period[4]'].text
-    min_per = 0 #最終的には30にする?
-
-    if per06to12 >= min_per || per12to18 >= min_per || per18to24 >= min_per
-      push = "今日は傘を持つのじゃ。#{weather}じゃからの。"
-    end
-
-
     events = client.parse_events_from(body)
-
     events.each {|event|
       case event
+      #メッセージが送信された場合の対応
       when Line::Bot::Event::Message
         case event.type
+        #text形式のメッセージが送られてきた場合
         when Line::Bot::Event::MessageType::Text
+          input = event.message['text']
+
+          url = "https://www.drk7.jp/weather/xml/13.xml"
+          #utf8に変換して取得(kconv)
+          xml = open(url).read.toutf8
+          doc = REXML::Document.new(xml)
+          min_per = 0 #最終的には30にする?
+
+          case input
+          when /.*(明日|あした).*/
+            push = "明日じゃな"
+
+          when /.*(テスト|てすと).*/
+            push = "テスト"
+          end
+    #
+    #       #東京都 -> 東京地方の情報取得
+    #       xpath = 'weatherforecast/pref/area[4]/info'
+    #       weather = doc.elements[xpath + '/weather'].text
+    #       per06to12 = doc.elements[xpath + '/rainfallchance/period[2]'].text
+    #       per12to18 = doc.elements[xpath + '/rainfallchance/period[3]'].text
+    #       per18to24 = doc.elements[xpath + '/rainfallchance/period[4]'].text
+    #
+    #
+    #
+    #
+    # if per06to12 >= min_per || per12to18 >= min_per || per18to24 >= min_per
+    #   push = "今日は傘を持つのじゃ。#{weather}じゃからの。"
+    # end
+
           message = {
               type: 'text',
               text: push
