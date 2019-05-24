c0771b8249a9506380988e53de21f1993bad6f27
hiramoto-kensuke <hiramoto.kensuke@moneyforward.co.jp>
2019-05-24 13:05:26 +0900

    天気APIテスト

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,5 +1,8 @@
 class LinebotController < ApplicationController
   require 'line/bot'
+  require 'open-uri'
+  require 'kconv'
+  require 'rexml/document'
 
   protect_from_forgery :except => [:callback]
 
@@ -21,6 +24,14 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           # event.message['text']：ユーザーから送られたメッセージ
           input = event.message['text']
+          url = "https://www.drk7.jp/weather/xml/13.xml"
+          xml = open(url).read.toutf8
+          doc = REXML::Document.new(xml)
+          xpath = 'weatherforecast/pref/area[4]/info'
+          weather = doc.elements[xpath + '/weather'].text
+
+          min_per = 0 #最終的には30に変更
+
 
           case input
             # 「明日」or「あした」というワードが含まれる場合
@@ -29,6 +40,15 @@ class LinebotController < ApplicationController
 
           when /.*(テスト|てすと).*/
             push = "テストてすと"
+
+          else
+            per06to12 = doc.elements[xpath + '/rainfallchance/period[2]'].text
+            per12to18 = doc.elements[xpath + '/rainfallchance/period[3]'].text
+            per18to24 = doc.elements[xpath + '/rainfallchance/period[4]'].text
+
+            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+              push = "今日は傘を持つのじゃ。#{weather}じゃからの"
+            end
 
           end
         end
